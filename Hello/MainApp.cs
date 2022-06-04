0d5856d8d630d3e1a6e84a65e558dc860efa921c
vipstard <vipstar_d@naver.com>
2022-06-04 23:01:15 +0900

    실행환경 설정 및 첫실습

---
+++ Hello/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            /* 매개변수가 들어오지 않았을때 */
+            if (args.Length == 0) 
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
