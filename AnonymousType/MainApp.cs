61901d131ad77a61a1663a2958b457f0ba7e28e2
vipstard <vipstar_d@naver.com>
2022-06-09 02:00:37 +0900

    Array

---
+++ AnonymousType/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AnonymousType
+{ /* 무명형식은 선언과 동시에 인스턴스 할당을 한다. 
+   * 인스턴스를 만들고 다시는 사용하지
+    않을때 무명형식이 요긴하다. 
+    (프로퍼티에 할당된 값 변경 불가능, 읽기만 가능하다는 이야기이다. ) 
+    참고로 LINQ와 함께 사용하면 아주 요긴하다. */
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "박상현", Age = 123 };
+            Console.WriteLine($"Name:{a.Name}, Age:{a.Age}");
+
+            var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60 } };
+
+            Console.Write($"Subject :{b.Subject}, Score: ");
+            foreach (var score in b.Scores)
+                Console.Write($"{score} ");
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PropertiesInInterface/MainApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace PropertiesInInterface
+{ /* 프로퍼티나 인덱서를 가진 인터페이스를 상속하는 클래스가 "반드시" 해당 프로퍼티와 인덱서를 구현해야 한다. */
+
+    interface INameValue
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+
+        string Value
+        {
+            get;
+            set;
+        }
+    }
+
+    class NamedValue : INameValue
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string Value
+        {
+            get;
+            set;
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            NamedValue name = new NamedValue()
+            { Name = "이름", Value = "박상현" };
+
+            NamedValue height = new NamedValue()
+            { Name = "키", Value = "177cm" };
+
+            NamedValue weight = new NamedValue()
+            {
+                Name = "몸무게",
+                Value = "90Kg"
+            };
+
+            Console.WriteLine($"{name.Name} : {name.Value}");
+            Console.WriteLine($"{height.Name} : {height.Value}");
+            Console.WriteLine($"{weight.Name}: {weight.Value}");
+
+
+        }
+    }
+}
