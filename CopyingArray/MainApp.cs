88a861fcfcf15fd47e3b80d49c06394ecce79884
vipstard <vipstar_d@naver.com>
2022-06-13 23:47:30 +0900

    제네릭프로그래밍

---
+++ CopyingArray/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CopyingArray
+{
+
+    class MainApp
+    {
+
+        /*일반화 메소드 : 메소드 이름 뒤 <T> 를 넣으면 형식매개변수 가 된다.*/
+
+        static void CopyArray<T>(T[] source, T[] target)
+        {
+            for (int i = 0; i < source.Length; i++)
+                target[i] = source[i];
+        }
+
+        static void Main(string[] args)
+        {
+            int[] source = { 1, 2, 3, 4, 5 };
+            int[] target = new int[source.Length];
+
+            CopyArray<int>(source, target);
+
+            foreach (int element in target)
+                Console.WriteLine(element);
+
+            string[] source2 = { "하나", "둘", "셋", "넷", "다섯" };
+            string[] target2 = new string[source2.Length];
+
+            CopyArray<string>(source2, target2);
+
+            foreach (string element in target2)
+                Console.WriteLine(element);
+
+
+        }
+    }
+}
---
+++ JaggedArray/MainApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JaggedArray
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[5] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
