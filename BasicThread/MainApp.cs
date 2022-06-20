f535555e72c391b1f471ebff09ef7300f5d191fd
vipstard <vipstar_d@naver.com>
2022-06-20 18:14:04 +0900

    병렬처리

---
+++ BasicThread/MainApp.cs
@@ -0,0 +1,30 @@
+﻿class MainApp
+{
+    static void DoSomething()
+    {
+        for (int i=0; i < 5; i++)
+        {
+            Console.WriteLine($"DoSomething : {i}");
+            Thread.Sleep(10);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Thread t1 = new Thread(new ThreadStart(DoSomething));
+
+        Console.WriteLine("Starting thread...");
+        t1.Start();
+
+        for (int i=0; i<5; i++)
+        {
+            Console.WriteLine($"Main : {i}");
+            Thread.Sleep(10);
+        }
+
+        Console.WriteLine("Watring untill thread stops...");
+        t1.Join();
+        Console.WriteLine("Finished");
+
+    }
+}
