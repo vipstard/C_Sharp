762c01533cb18661f2db19a69f1c01289d911ff7
vipstard <vipstar_d@naver.com>
2022-06-22 14:03:54 +0900

    ASP.NET Core 기초

---
+++ ASP.NET Core/AspnetCoreStudy/AspnetCoreStudy/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspnetCoreStudy.Models;
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        //http:www.example.com/Home/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
