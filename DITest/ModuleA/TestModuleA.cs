83e3efbbdfff855e5f4d8f9fdc7a25b6159952a8
vipstar <vipstar_d@naver.com>
2022-06-30 18:30:51 +0900

    DI

---
+++ DITest/ModuleA/TestModuleA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class TestModuleA
+    {
+
+        public void Test1()
+        {
+            throw new ArgumentException();
+        }
+    }
+}
