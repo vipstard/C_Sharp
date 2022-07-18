c96d15dcfd9c0d55a9094a2815745af4e9ce5034
vipstar <vipstar_d@naver.com>
2022-07-18 17:22:54 +0900

    async await

---
+++ C#/Async/Async/MainApp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class MainApp
+    {
+        async static private void MyMethodAsyunc(int count)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+
+            await Task.Run(async() =>{
+                for(int i=1; i<=count; i++)
+                {
+                    Console.WriteLine($"{i}/{count} ...");
+                    await Task.Delay(100);
+                }
+        });
+
+        Console.WriteLine( "G");
+        Console.WriteLine( "H");
+             
+          static void Caller()
+            {
+                Console.WriteLine("A");
+                Console.WriteLine("B");
+
+                MyMethodAsyunc(3);
+
+                Console.WriteLine("E");
+                Console.WriteLine("F");
+
+            }
+
+        static void Main(string[] args)
+        {
+                Caller();
+
+                Console.ReadLine();//프로그램 종료 방지
+        }
+    }
+}
