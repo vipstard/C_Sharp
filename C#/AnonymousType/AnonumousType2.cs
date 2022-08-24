33df0a80c4d969d9625dcc4051427742d89b30a7
vipstar <vipstar_d@naver.com>
2022-08-24 16:02:34 +0900

    익명함수

---
+++ C#/AnonymousType/AnonumousType2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AnonymousType
+{
+     class AnonumousType2
+    {
+        static void Main(string[] args)
+        {
+            var a = new { Name = "박상현", Age = 123 };
+            Console.WriteLine($"Name:{a.Name}, Age: {a.Age}");
+
+            var b = new { Subject = "수학", Scores = new int[] { 90, 80, 70, 60 } };
+
+            Console.Write($"Subject:{b.Subject}, Scores: ");
+            foreach (var score in b.Scores)
+                Console.WriteLine($"{score} ");
+
+            Console.WriteLine();
+
+        }
+    }
+}
