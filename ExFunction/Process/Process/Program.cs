21a1e44052e44634edebff42fcc36d08109a48fe
vipstar <vipstar_d@naver.com>
2022-11-23 11:03:38 +0900

    Process

---
+++ ExFunction/Process/Process/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Process
+{
+    using System.Diagnostics;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "sc query wnms service";
+
+            //CMD 접근
+            Process myProcess = Process.Start("cmd.exe", command); 
+            
+            Console.WriteLine(myProcess.Responding);
+
+            if (myProcess.Responding) //Running True 상태
+            {
+                // 프로세스에 명령어 날리기
+                // Ex) Process.Start("Path + FileName Or FileName", "Command");
+
+
+            }
+
+        }
+    }
+}
