0773fe0feefac08d942cd0ea4e3822477d94c8d9
vipstar <vipstar_d@naver.com>
2022-11-28 14:53:47 +0900

    LinqParser

---
+++ ExFunction/SCL_Parser/SCL_Parser/LInqParser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SCL_Parser
+{
+    public class LInqParser
+    {
+        public static void Main()
+        {
+
+            String path = "C:\\nms4sa\\database\\TEST.cid";
+            XDocument xd = XDocument.Load(path);
+            XElement root = xd.Document.Root;
+
+            XNamespace ns = XDocument.Load(path).Root.GetDefaultNamespace();
+
+            var connectedAps = root
+                .Descendants(ns+ "ConnectedAP")
+                .Select(x => new ConnectedAp()
+                {
+                    iedName = x.Attribute("iedName").Value,
+                    ip = x.Descendants(ns +"P").First().Value
+                }).ToList();
+
+
+            foreach (var ca in connectedAps)
+            {
+             Console.WriteLine($"{ca.iedName}, {ca.ip}");   
+            }
+        }
+
+        public class ConnectedAp
+        {
+            public String iedName { get; set; }
+            public String ip { get; set; }
+
+            public ConnectedAp()
+            {
+            }
+          
+            public ConnectedAp(String iedName, String ip)
+            {
+                this.iedName = iedName;
+                this.ip = ip;
+            }
+        }
+    }
+}
+
+// TEST.cid
+//<? xml version = "1.0" encoding = "utf-8" ?>
+//    < SCL xmlns = "http://www.iec.ch/61850/2003/SCL" xmlns: xsi = "http://www.w3.org/2001/XMLSchema-instance" xsi: schemaLocation = "http://www.iec.ch/61850/2003/SCL SCL.xsd" >
+//    < Header id = "NMS_IEC61850_SERVER" nameStructure = "IEDName" />
+//    < Communication >
+//    < SubNetwork name = "SubNetwork1" type = "8-MMS" >
+//    < ConnectedAP apName = "S1" iedName = "D000_NMS3" >
+//    < Address >
+//    < P type = "IP" > 192.168.56.1 </ P >
+//    < P type = "IP-SUBNET" > 255.255.255.0 </ P >
+//    < P type = "IP-GATEWAY" > 192.168.111.1 </ P >
+//    < P type = "OSI-AP-Title" > 1,1,9999,1 </ P >
+//    < P type = "OSI-AE-Qualifier" > 6 </ P >
+//    < P type = "OSI-PSEL" > 00000001 </ P >
+//    < P type = "OSI-SSEL" > 0001 </ P >
+//    < P type = "OSI-TSEL" > 0001 </ P >
+//    </ Address >
+//    </ ConnectedAP >
+//    </ SubNetwork >
+//    </ Communication >
+//    </ SCL >
---
+++ ExFunction/SCL_Parser/SCL_Parser/program2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace SCL_Parser
+{
+    //XML 파일의 모든 태그 값을 가져오기
+    internal class program2
+    {
+
+        public static void Main()
+        {
+            String path = "C:\\nms4sa\\database\\TEST.cid";
+            XNamespace ns = XDocument.Load(path).Root.GetDefaultNamespace();
+            XDocument doc = XDocument.Load(path); //Loads the document by looking for the given path
+
+            // 전체 출력
+
+            List<XElement> elements = doc.Descendants().ToList(); //Extracts a list of every descendant node of the starting element.
+
+            //foreach (XElement element in elements)
+            //{
+            //    Console.WriteLine(element);
+            //}
+
+            // element 출력
+            XElement response = doc.Elements().FirstOrDefault();
+            //Console.WriteLine(response);
+
+            List<XElement> ConnectedAp = doc.Descendants("P").ToList();
+
+            var ConnectAp = XDocument.Load(path).Descendants(ns + "ConnectedAP").ToList();
+            foreach (var ca in ConnectAp)
+            {
+
+               
+
+            }
+
+
+            List<ConnectedAp> CAP = ConnectedAp.Select(x => Deserialize<ConnectedAp>(x.ToString())).ToList();
+            foreach (var ca in ConnectedAp)
+            {
+                Console.WriteLine(ca);
+            }
+            
+        }
+
+
+        public static string Serialize<T>(T dataToSerialize)
+        {
+            try
+            {
+                var stringwriter = new System.IO.StringWriter();
+                var serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(stringwriter, dataToSerialize);
+                return stringwriter.ToString();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static T Deserialize<T>(string xmlText)
+        {
+            try
+            {
+                var stringReader = new System.IO.StringReader(xmlText);
+                var serializer = new XmlSerializer(typeof(T));
+                return (T)serializer.Deserialize(stringReader);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+
+    [XmlRoot(ElementName = "SCL", Namespace = "http://www.iec.ch/61850/2003/SCL")]
+    public class ConnectedAp
+    {
+        public String iedName { get; set; }
+        public String ip { get; set; }
+
+        public ConnectedAp(){}
+        public ConnectedAp(String iedName, String ip)
+        {
+            this.iedName = iedName;
+            this.ip = ip;
+        }
+    }
+
+}
+
