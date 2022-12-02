dd52ec78abcc22f1d2d986da5828600978b7839f
vipstar <vipstar_d@naver.com>
2022-12-02 17:54:34 +0900

    StreamWriter

--- ExFunction/FileStream_SclParesr/FileStream_SclParesr/Controller/HomeController.cs
+++ ExFunction/FileStream_SclParesr/FileStream_SclParesr/Controller/HomeController.cs
@@ -16,42 +16,48 @@ namespace FileStream_SclParesr.Controller
         }
 
         [HttpPost("/Home/upload")]
-        public  IActionResult upload(IFormFile testFile)
+        public async Task<IActionResult> upload(IFormFile testFile)
         {
-            //1.  stringBuilder 사용해서 filestream 사용
-            var result = new StringBuilder();
-            using (var reader = new StreamReader(testFile.OpenReadStream()))
-            {
-                while (reader.Peek() >= 0)
-                    result.AppendLine(await reader.ReadLineAsync());
-            }
-            //Debug.WriteLine(result.ToString());
+            ////1.  stringBuilder 사용해서 filestream 사용
+            //var result = new StringBuilder();
+            //using (var reader = new StreamReader(testFile.OpenReadStream()))
+            //{
+            //    while (reader.Peek() >= 0)
+            //        result.AppendLine(await reader.ReadLineAsync());
+            //}
+            ////Debug.WriteLine(result.ToString());
 
 
-            //2. IFormFile -> XML 즉시변환
-            using (MemoryStream str = new MemoryStream())
-            {
-                await testFile.CopyToAsync(str);
-                str.Position = 0;
+            ////2. IFormFile -> XML 즉시변환
+            //using (MemoryStream str = new MemoryStream())
+            //{
+            //    await testFile.CopyToAsync(str);
+            //    str.Position = 0;
 
-                var xml = XDocument.Load(str);
-                XElement root = xml.Document.Root;
-                XNamespace ns = root.GetDefaultNamespace();
+            //    var xml = XDocument.Load(str);
 
-                var connectedAps = root
-                    .Descendants(ns + "ConnectedAP")
-                    .Select(x => new ConnectedAp()
-                    {
-                        iedName = x.Attribute("iedName").Value,
-                        ip = x.Descendants(ns + "P").First().Value
-                    }).ToList();
+            //    XElement root = xml.Document.Root;
+            //    XNamespace ns = root.GetDefaultNamespace();
+
+            //    var connectedAps = root
+            //        .Descendants(ns + "ConnectedAP")
+            //        .Select(x => new ConnectedAp()
+            //        {
+            //            iedName = x.Attribute("iedName").Value,
+            //            ip = x.Descendants(ns + "P").First().Value
+            //        }).ToList();
 
 
-                foreach (var ca in connectedAps)
-                {
-                    Debug.WriteLine($"{ca.iedName}, {ca.ip}");
-                }
-            }
+            //    foreach (var ca in connectedAps)
+            //    {
+            //        Debug.WriteLine($"{ca.iedName}, {ca.ip}");
+            //    }
+            //}
+           
+
+            //3. File Path => IFormFile 파일 처리전 서버의 메모리 또는 디스크에 버퍼링 됌.
+            // 작업 메서드내에서 IFormFile 콘텐츠는 Stream으로 접근 할 수 있다.
+
            
             return Ok();
         }
---
+++ ExFunction/StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace StreamWriter
+{
+    using System.IO;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer;
+            writer = File.AppendText("경로"); // 이어쓰기
+            //writer = File.CreateText("경로");  덮어쓰기
+
+            writer.WriteLine("content");
+
+            writer.Close();
+    }
+}
