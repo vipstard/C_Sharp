b09538d94d6b877c5e8f265fa794b4a466cd92d5
vipstar <vipstar_d@naver.com>
2023-01-04 17:08:22 +0900

    DateTime

---
+++ ExFunction/DateTimeAnd Calendar/DateTimeAnd Calendar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace DateTimeAnd_Calendar
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = 50;
+            DateTime today = DateTime.Now;
+            DateTime result = today.AddDays(-30);
+
+            String date = string.Format("{0:yyyy-MM-dd HH:mm:ss}", result);
+
+            Console.WriteLine("today : {0:yyyy-MM-dd HH:mm:ss}", today);
+            Console.WriteLine(date);
+           
+        }
+    }
+}
