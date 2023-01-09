5958a95ded21bbd8b345a976531444e76a8e09e4
vipstar <vipstar_d@naver.com>
2023-01-09 19:06:41 +0900

    Linq .distinct()

---
+++ ExFunction/LInq_Ex/LInq_Ex/DisTinct.cs
@@ -0,0 +1,44 @@
+﻿namespace LInq_Ex
+{
+    public class DisTinct
+    {
+        // Linq 중복제거 
+        static void Main(string[] args)
+        {
+      
+            List<Person> list = new List<Person>()
+          {
+              new Person("John", 24),
+              new Person("John", 24),
+              new Person("Park", 56),
+              new Person("Park", 56),
+              new Person("Lily", 23),
+              new Person("Lily", 23),
+              new Person("Roanldo", 30),
+              new Person("Roanldo", 30)
+          };
+
+            // 익명타입 사용
+          var queryResult = 
+              (from A in list select A)
+              .Select(A=>new {A.Name, A.age})
+              .Distinct();
+
+          foreach (var result in queryResult)
+          {
+              Console.WriteLine($"{result.Name} / {result.age}");
+            }
+
+          Console.WriteLine();
+
+          var methodResult = list.Select(A=>new {A.Name, A.age}).Distinct();
+
+          foreach (var result in methodResult)
+          {
+              Console.WriteLine($"{result.Name}  / {result.age}");
+            }
+          
+
+        }
+    }
+}
---
+++ ExFunction/LInq_Ex/LInq_Ex/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInq_Ex
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public int age { get; set; }
+
+        public Person()
+        {
+            
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return this.Name == ((Person)obj).Name && this.age == ((Person)obj).age;
+        }
+    }
+}
