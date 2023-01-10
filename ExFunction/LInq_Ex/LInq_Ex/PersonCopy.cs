10e92c2746916674e5f5c15cd4e246723697548f
vipstar <vipstar_d@naver.com>
2023-01-10 10:35:29 +0900

    LInq Select()

--- ExFunction/LInq_Ex/LInq_Ex/PersonCopy.cs
+++ ExFunction/LInq_Ex/LInq_Ex/PersonCopy.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace LInq_Ex
 {
-    public class Person
+    public class PersonCopy
     {
         public String Name { get; set; }
         public int age { get; set; }
 
         public override string ToString()
         {
-            return "Name : " +   Name +  ",  Age : " +  age;
+            return "Name : " + Name + ",  Age : " + age;
         }
 
-        public Person()
+        public PersonCopy()
         {
-            
+
         }
-        public Person(string name, int age)
+        public PersonCopy(string name, int age)
         {
             Name = name;
             this.age = age;
---
+++ ExFunction/LInq_Ex/LInq_Ex/Select.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInq_Ex
+{
+    public class Select
+    {
+
+        static void Main(String[] args)
+        {
+            List<Person> list = new List<Person>()
+            {
+                new Person("John", 24),
+                new Person("John", 24),
+                new Person("Park", 56),
+                new Person("Park", 56),
+                new Person("Lily", 23),
+                new Person("Lily", 23),
+                new Person("Roanldo", 30),
+                new Person("Roanldo", 30)
+            };
+
+            // Person -> PersonCopy 타입 변경
+            List<PersonCopy> methodResult = list.Select(p=> new PersonCopy()
+            {
+                Name = p.Name.ToLower(),
+                age = p.age * 10 // 프로퍼티 값 변경
+            }).ToList();
+
+            foreach (var result in methodResult)
+            {
+                Console.WriteLine(result.ToString());
+
+            }
+         
+        }
+        
+
+
+    }
+}
