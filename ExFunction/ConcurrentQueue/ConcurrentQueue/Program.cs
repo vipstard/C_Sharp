45e84b50266859cbfee5b943928c8d70ee29f394
vipstar <vipstar_d@naver.com>
2023-01-10 13:27:29 +0900

    Concurrent Queue

---
+++ ExFunction/ConcurrentQueue/ConcurrentQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentQueue
+{
+    public class Program
+    {
+        // ConcurrentQueue 에는 Dequeue() 메서드가 없고 대신 TryDequeue() 메서드를 사용한다
+        //또한 마찬가지로 ConcurrentQueue에서는 Peek() 메서드 대신 TryPeek() 메서드를 사용한다.
+
+        static void Main(string[] args)
+        {
+            var queue = new ConcurrentQueue<int>();
+
+            // Queue 에 데이터를 넣는 쓰레드
+            Task tEnq = Task.Run(() =>
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    queue.Enqueue(i);
+                    Task.Delay(100);
+                }
+            });
+
+            // Queue에서 데이터를 읽는 쓰레드
+            Task tDeq = Task.Run(() =>
+            {
+                int n = 0;
+                int result;
+                while (n<1000)
+                {
+                    if (queue.TryDequeue(out result))
+                    {
+                        Console.WriteLine(result);
+                        n++;
+                    }
+                    Task.Delay(100);
+                }
+            });
+
+            Task.WaitAll(tEnq, tDeq);
+        }
+    }
+}
---
+++ ExFunction/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        //링크드 리스트는 특정 노드에서 노드를 삽입, 삭제하기 편리 하지만 ( O(1) ),
+        //특정 노드를 검색하기 위해서는 O(n)의 시간이 소요된다.
+
+        static void Main(string[] args)
+        {
+            LinkedList<String> list = new LinkedList<String>();
+
+            list.AddLast("Apple");
+            list.AddLast("Banana");
+            list.AddLast("Lemon");
+
+            LinkedListNode<String> node = list.Find("Banana");
+            LinkedListNode<String> newNode = new LinkedListNode<string>("Grape");
+
+            list.AddAfter(node, newNode); //Banana 뒤에 Grape를 추가한다.
+
+            // Enumerator 리스트 출력
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }
+
+            Console.WriteLine();
+            // 람다식 출력
+            list.ToList<String>().ForEach(l=> Console.WriteLine(l));
+
+
+
+        }
+    }
+}
