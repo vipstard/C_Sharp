2c5176e1f2bd7a0b716a3b4a6ac49ce9e1ade5f0
vipstar <vipstar_d@naver.com>
2023-01-10 18:43:57 +0900

    BinarySearchTree

---
+++ ExFunction/BinarySeachTree/BinarySeachTree/BST.cs
@@ -0,0 +1,81 @@
+﻿namespace BinarySearchTree
+{
+    // BinarySearchTree Class
+    public class BST<T>
+    {
+        private BinaryTreeNode<T> root = null;
+        private Comparer<T> comparer = Comparer<T>.Default;
+        
+        //1. 루트 노드의 키와 찾고자 하는 값을 비교한다. 찾고자 하는 값이라면 탐색을 종료한다.
+        //2. 찾고자 하는 값이 루트 노드의 키보다 작다면 왼쪽 서브 트리로 탐색을 진행한다.
+        //3. 찾고자 하는 값이 루트노드의 키보다 크다면 오른쪽 서브트리로 탐색을 진행한다. 
+        public void Insert(T val)
+        {
+            BinaryTreeNode<T> node = root;
+            
+            if(node ==null)
+            {
+                root = new BinaryTreeNode<T>(val);
+                return;
+            }
+
+            while (node!=null)
+            {
+                // x 가 크면 -1, 같으면 0, x가 작으면 1
+                int result = comparer.Compare(node.Data, val);
+                if (result == 0)
+                {
+                    return;
+                }
+                else if (result > 0)
+                {
+                    if (node.Left == null)
+                    {
+                        node.Left = new BinaryTreeNode<T>(val);
+                        return;
+                    }
+
+                    node = node.Left;
+                }
+
+                else
+                {
+                    if (node.Right == null)
+                    {
+                        node.Right = new BinaryTreeNode<T>(val);
+                        return;
+                    }
+
+                    node = node.Right;
+                }
+            }
+        }
+
+
+        public void PreOrderTraversal()
+        {
+            PreOrderRecursive(root);
+        }
+
+        private void PreOrderRecursive(BinaryTreeNode<T> node)
+        {
+            if (node == null) return;
+            Console.WriteLine(node.Data);
+            PreOrderRecursive(node.Left);
+            PreOrderRecursive(node.Right);
+        }
+
+    }
+
+    internal class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T>  Left { get; set; }
+        public BinaryTreeNode<T>  Right { get; set; }
+
+        public BinaryTreeNode(T data)
+        {
+            this.Data = data;
+        }
+    }
+}
---
+++ ExFunction/BinarySeachTree/BinarySeachTree/Program.cs
@@ -0,0 +1,40 @@
+﻿using BinarySearchTree;
+
+namespace BinarySeachTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BST<int> bst = new BST<int>();
+
+            bst.Insert(4);
+            bst.Insert(2);
+            bst.Insert(6);
+            bst.Insert(1);
+            bst.Insert(3);
+            bst.Insert(7);
+            bst.Insert(10);
+
+            bst.PreOrderTraversal();
+            Console.WriteLine();
+
+            // .NET이 이진검색트리를 클래스를 public으로 제공하지 않지만,
+            // 내부적으로 BST를 사용해 SortedDictionary를 구현하고 있다.
+
+            SortedDictionary<int, string> tmap = new SortedDictionary<int, string>();
+            tmap.Add(1001, "Tom");
+            tmap.Add(1023, "John");
+            tmap.Add(1010, "Irina");
+
+            // Key 1010의 데이타 읽기
+            string name1010 = tmap[1010];
+
+            // Iterator 사용
+            foreach (KeyValuePair<int, string> kv in tmap)
+            {
+                Console.WriteLine("{0}:{1}", kv.Key, kv.Value);
+            }
+        }
+    }
+}
