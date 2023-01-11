11765db3f789ef36084d5097c9f1468c1a17186f
vipstar <vipstar_d@naver.com>
2023-01-11 11:21:53 +0900

    linq INdex, OfType

---
+++ ExFunction/LInq_Ex/LInq_Ex/Index.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInq_Ex
+{
+    internal class Index
+    {
+        static void Main(string[] args)
+        {
+            List<string> strLi = new List<string>()
+            {
+                "Java", "C Sharp", "C++", "JavaScript", "React"
+            };
+
+            var linqMethodResult = strLi
+                .Select((item, index) => new  // 첫 번째 호출. 인덱스와 요소를 가지는 익명 타입의 객체 생성
+                {
+                    index,
+                    subject = item
+                })
+                .Where(item => item.subject.StartsWith("C")) // "C"로 시작하는 요소를 필터
+                .Select(item => new                          // 필터된 항목을 추출
+                {
+                    index = item.index,
+                    subject = item.subject
+                });
+
+            foreach (var obj in linqMethodResult)
+                Console.WriteLine("index: " + obj.index + " / Subject: " + obj.subject);
+        }
+    }
+}
---
+++ ExFunction/LInq_Ex/LInq_Ex/OfType().cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInq_Ex
+{
+    internal class OfType__
+    {
+        static void Main(String[] args)
+        {
+            // Object 타입 List는 모든 타입의 값을 가질 수 있다.
+            List<Object> allTypeList = new List<object>()
+            {
+                "C#", 222, true, "Java", 333, false, "Vue.js"
+            };
+
+            List<string> strList = allTypeList.OfType<string>().ToList();
+
+            foreach (var val in strList)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
