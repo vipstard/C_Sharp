85e11c0d1654820e6aa10737801b214751bedc75
vipstar <vipstar_d@naver.com>
2023-01-17 14:23:42 +0900

    linq

---
+++ ExFunction/LInq_Ex/LInq_Ex/GroupBy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LInq_Ex
+{
+    public class GroupBy
+    {
+        static void Main(String[] args)
+        {
+            var GroupByMethodResult = Person.GetPersons()
+                .GroupBy(p => p.Grade)
+                .Select(p => new
+                {
+                    Grade = p.Key,
+                    Count = p.Count()
+                });
+
+            Console.WriteLine("메서드 구문");
+
+            foreach (var group in GroupByMethodResult)
+            {
+                Console.WriteLine(group.Grade + " 학년의 인원 수 : " + group.Count);
+            }
+        }
+    }
+}
--- ExFunction/LInq_Ex/LInq_Ex/Person.cs
+++ ExFunction/LInq_Ex/LInq_Ex/Person.cs
@@ -10,21 +10,39 @@ namespace LInq_Ex
     {
         public String Name { get; set; }
         public int Age { get; set; }
+        public int Grade { get; set; }
 
         public override string ToString()
         {
-            return "Name : " +   Name +  ",  Age : " + Age;
+            return "Name : " + Name + ",  Age : " + Age + ", Grade : " + Grade;
         }
 
         public Person()
         {
-            
         }
+
         public Person(string name, int Age)
         {
             Name = name;
             this.Age = Age;
         }
 
+        public static List<Person> GetPersons()
+        {
+            return new List<Person>()
+            {
+                new Person { Name = "1", Age = 20, Grade = 1 },
+                new Person { Name = "1", Age = 21, Grade = 1 },
+                new Person { Name = "1", Age = 22, Grade = 1 },
+                new Person { Name = "1", Age = 23, Grade = 2 },
+                new Person { Name = "1", Age = 24, Grade = 2 },
+                new Person { Name = "1", Age = 25, Grade = 1 },
+                new Person { Name = "1", Age = 26, Grade = 3 },
+                new Person { Name = "1", Age = 27, Grade = 3 },
+                new Person { Name = "1", Age = 28, Grade = 3 },
+                new Person { Name = "1", Age = 29, Grade = 3 },
+                new Person { Name = "1", Age = 30, Grade = 4 }
+            };
+        }
     }
 }
