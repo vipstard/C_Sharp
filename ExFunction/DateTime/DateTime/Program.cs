94386c796155aa4a9a278a268a1377095787b2e7
vipstar <vipstar_d@naver.com>
2023-03-31 10:10:10 +0900

    DateTime ex

---
+++ ExFunction/DateTime/DateTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2023-03-30 18:52:54 를 2023-03-30 오후 6:52;54로 바꾸고싶다.
+
+            String timeStamp1 = "2023-03-30 18:52:54.745";
+            System.DateTime time = System.DateTime.Parse(timeStamp1);
+            string outputTime = time.ToString("yyyy-MM-dd tt h:mm:ss");
+
+            Console.WriteLine(outputTime);
+
+
+
+        }
+    }
+}
