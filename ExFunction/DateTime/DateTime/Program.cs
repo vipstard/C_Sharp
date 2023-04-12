9112d6784d1ea385a27bf37199c2c02598698787
vipstar <vipstar_d@naver.com>
2023-04-12 18:08:03 +0900

    ExtendedLambda"

--- ExFunction/DateTime/DateTime/Program.cs
+++ ExFunction/DateTime/DateTime/Program.cs
@@ -7,7 +7,6 @@ namespace DateTime
         static void Main(string[] args)
         {
             // 2023-03-30 18:52:54 를 2023-03-30 오후 6:52;54로 바꾸고싶다.
-
             String timeStamp1 = "2023-03-30 18:52:54.745";
             System.DateTime time = System.DateTime.Parse(timeStamp1);
             string outputTime1 = time.ToString("yyyy-MM-dd tt h:mm:ss");
---
+++ ExFunction/NewFeatureCShapr10/NewFeatureCShapr10/EnhancedLamda.cs
@@ -0,0 +1,19 @@
+﻿namespace NewFeatureCShapr10
+{
+    internal class EnhancedLamda
+    {
+        static void Main(string[] args)
+        {
+            // 기존 C# 9 에서는 Delegate 타입을 명시적으로 지정해줘야 했음.
+            Func<string, int> stringConvertIntFunc = (string s) => int.Parse(s);
+            int n1 = stringConvertIntFunc("101");
+            Console.WriteLine(n1);
+
+            // C# 10 에서는 var를 사용하여 컴파일러가 Delegate 타입을 유추 할 수 있도록 함.
+            // 즉 람다식 유추 기능이 향상되었다.
+            var stringConvertIntFunc2 = (string s) => int.Parse(s);
+            var n2 = stringConvertIntFunc2("101");
+            Console.WriteLine(n2);
+        }
+    }
+}
