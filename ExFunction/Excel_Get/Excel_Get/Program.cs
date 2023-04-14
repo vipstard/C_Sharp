193755b599566310b3dab9a5e54dac3d44f1ac20
vipstar <vipstar_d@naver.com>
2023-04-14 10:52:06 +0900

    ExcelGet

---
+++ ExFunction/Excel_Get/Excel_Get/Program.cs
@@ -0,0 +1,16 @@
+﻿global using Excel = Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+
+
+namespace Excel_Get
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application excel = new Excel.Application();
+            Excel.Workbook workbook = excel.Workbooks.Open(@"C:\nms4sa\database\iet\");
+        }
+    }
+}
+
