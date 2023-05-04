73846792d69e791152450b497d9d762adf67cb8c
vipstar <vipstar_d@naver.com>
2023-05-04 11:33:16 +0900

    Service 프로그램 재시작

--- ExFunction/GetProcessInfo/GetProcessInfo/Program.cs
+++ ExFunction/GetProcessInfo/GetProcessInfo/Program.cs
@@ -1,12 +1,21 @@
-﻿namespace GetProcessInfo
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace GetProcessInfo
 {
-	internal class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-			GetPidAndStatus();
+			string Path = @"C:\nms4sa\PacketCap_DNP.exe";
+			RestartService();
+			Process.Start(Path);
 		}
 
+		/// <summary>
+		/// Process 동작 중 or 정지 상태 판단
+		/// </summary>
 		private static void GetPidAndStatus()
 		{
 			System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("PacketCap_DNP");
@@ -20,6 +29,29 @@
 			}
 
 			Console.WriteLine($"PID: {pid}, 동작 상태: {(isRunning ? "동작 중" : "종료됨")}");
+
+		}
+
+		/// <summary>
+		/// 서비스 재시작
+		/// </summary>
+		private static void RestartService()
+		{
+			string processName = "PacketCap_DNP"; // 재시작할 프로세스 이름
+
+			// 서비스 컨트롤러 생성
+			ServiceController controller = new ServiceController(processName);
+
+			// 서비스 중지
+			if (controller.Status != ServiceControllerStatus.Stopped)
+			{
+				controller.Stop();
+				controller.WaitForStatus(ServiceControllerStatus.Stopped);
+			}
+
+			// 서비스 시작
+			controller.Start();
+			controller.WaitForStatus(ServiceControllerStatus.Running);
 
 		}
 	}
