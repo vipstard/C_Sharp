e09cd36e8f7f3da00a9901445f394ba2bd23974b
vipstar <vipstar_d@naver.com>
2023-05-10 14:35:19 +0900

    OpenXml MmsSheet

---
+++ ExFunction/OpenXml/OpenXml/Model/IET_MMS.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MDAS.Model.config
+{
+    [Table("iet_mms")]
+    public class IET_MMS
+    {
+        public int Id { get; set; }
+
+        [Column("device_id")]
+        public int? DeviceId { get; set; }
+
+        public string? FileName { get; set; }
+
+        public string? Description { get; set; }
+
+        [NotMapped] 
+        public string? GooseSend { get; set; }
+
+        [Column("data_type")]
+        public string? DataType { get; set; }
+
+        public string? Address { get; set; }
+
+        public IET_MMS()
+        {
+        }
+    }
+}
