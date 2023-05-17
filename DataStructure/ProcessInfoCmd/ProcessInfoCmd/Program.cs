f314c1c625adb92ae777ff228485dfa2abe92f62
vipstar <vipstar_d@naver.com>
2023-05-17 11:23:22 +0900

    Process Cmd

--- DataStructure/ProcessInfoCmd/ProcessInfoCmd/Program.cs
+++ DataStructure/ProcessInfoCmd/ProcessInfoCmd/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ProcessInfoCmd
+﻿using System;
+using System.Diagnostics;
+
+namespace ProcessInfoCmd
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int setting = 50;
+            string command = "C://nms4sa// " + setting.ToString();
+            MmsScan(command);
+        }
+
+        static private void MmsScan(string command)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.RedirectStandardOutput = true;
+            processInfo.UseShellExecute = false;
+            processInfo.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = processInfo;
+            process.Start();
+
+            string output = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+
+            Debug.WriteLine(output);
+
         }
     }
 }
