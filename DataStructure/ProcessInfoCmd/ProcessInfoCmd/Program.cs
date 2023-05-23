bb78204ba31888e75218d90b868dc6898da11860
vipstar <vipstar_d@naver.com>
2023-05-23 15:20:05 +0900

    DefaultEthernetSwtichPortOrder

--- DataStructure/ProcessInfoCmd/ProcessInfoCmd/Program.cs
+++ DataStructure/ProcessInfoCmd/ProcessInfoCmd/Program.cs
@@ -7,28 +7,66 @@ namespace ProcessInfoCmd
     {
         static void Main(string[] args)
         {
-            int setting = 50;
-            string command = "C://nms4sa// " + setting.ToString();
-            MmsScan(command);
+            //int setting = 50;
+            //string command = "C://nms4sa// " + setting.ToString();
+            //MmsScan(command);
+            int[] list = DefaultEthernetSwitchPortOrder();
+
+            Console.WriteLine("\n\n===========Print==========");
+            foreach (var i in list)
+            {
+	            Console.WriteLine(i);
+            }
+
+
         }
 
-        static private void MmsScan(string command)
+        static private int[] DefaultEthernetSwitchPortOrder()
         {
-            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
-            processInfo.RedirectStandardOutput = true;
-            processInfo.UseShellExecute = false;
-            processInfo.CreateNoWindow = true;
+	        int reverseStartNum = 0;
+	        int[] list = new int[24];
+	        int idx = 0;
 
-            Process process = new Process();
-            process.StartInfo = processInfo;
-            process.Start();
+			for (int i = 1; i <= 9; i += 4)
+	        {
+		        Console.WriteLine(i + "===>");
+                list[idx++] = i;
 
-            string output = process.StandardOutput.ReadToEnd();
+		        for (int j = i + 1; j < i + 4; j++)
+		        {
+			        Console.WriteLine(j);
+			        list[idx++] = j;
+			        if (j == i + 3)
+			        {
+				        reverseStartNum = j + 12;
+			        }
+		        }
 
-            process.WaitForExit();
+		        for (int k = reverseStartNum; k > reverseStartNum - 4; k--)
+		        {
+			        Console.WriteLine(k);
+			        list[idx++] = k;
+		        }
+	        }
+            return list;
+		}
+        //static private void MmsScan(string command)
+        //{
+        //    ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+        //    processInfo.RedirectStandardOutput = true;
+        //    processInfo.UseShellExecute = false;
+        //    processInfo.CreateNoWindow = true;
 
-            Debug.WriteLine(output);
+        //    Process process = new Process();
+        //    process.StartInfo = processInfo;
+        //    process.Start();
 
-        }
+        //    string output = process.StandardOutput.ReadToEnd();
+
+        //    process.WaitForExit();
+
+        //    Debug.WriteLine(output);
+
+        //}
     }
 }
---
+++ DataStructure/pcapFile_ReadToWriteExport/pcapFile_ReadToWriteExport/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace pcapFile_ReadToWriteExport
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+		static public string Export(/*[FromBody] SingleFileExportParam param*/)
+		{
+			//Save at Windows Temporary Folder
+			string Root = Path.GetTempPath() + "/";
+			string fileName = Root;
+			return fileName;
+		}
+
+	}
+}
