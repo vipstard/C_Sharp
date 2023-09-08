ebd80451bf4571742a80c45fb8a63e1a835ba707
vipstar <vipstar_d@naver.com>
2023-09-08 12:02:31 +0900

    optimization

--- ExFunction/ProcessMonitoring/ProcessMonitoring/EventHandle.cs
+++ ExFunction/ProcessMonitoring/ProcessMonitoring/EventHandle.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Management;
-using System.Text;
-using System.Threading.Tasks;
 using proc_mon;
 using proc_mon.Model;
 using ProcessMonitoring.Model;
@@ -13,89 +9,90 @@ namespace ProcessMonitoring
 {
     public class EventHandle
     {
-        private  List<ProcessInfo> _processInfos = null;
-        private  DbManager _dbManager = new DbManager();
+        private List<ProcessInfo> _processInfos = null;
+        private DbManager _dbManager = new DbManager();
 
-         public void ProcessStartedHandler(object sender, EventArrivedEventArgs e)
+        public void ProcessStartedHandler(object sender, EventArrivedEventArgs e)
         {
-            _processInfos = _dbManager.GetProcessInfo();
+            LoadProcessInfo();
+
             string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
             int processId = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value);
+            processName = GetNormalizedProcessName(processName);
 
+            ProcessInfo processInfo = _processInfos.Find(pi => pi.Name == processName);
 
-            foreach (ProcessInfo processInfo in _processInfos)
+            if (processInfo != null)
             {
-                processName = processName.Split('.')[0];
-                if (processInfo.Name == processName)
-                {
-                    Console.WriteLine($"프로세스 시작: {processName}, Pid: {processId}");
-                    processInfo.Status = 1;
-                    processInfo.Pid = processId;
-
-                    _dbManager.InsertLog(new ProcessEvent(processName, 1, 0));
-                    break;
-                }
+                Console.WriteLine($"프로세스 시작: {processName}, Pid: {processId}");
+                processInfo.Status = 1;
+                processInfo.Pid = processId;
+                _dbManager.InsertLog(new ProcessEvent(processName, 1, 0));
             }
 
-            // 프로세스 시작 이벤트가 발생할 때 DB 업데이트
-            _dbManager.UpdateProcessInfo(_processInfos);
+            UpdateProcessInfo();
         }
 
         public void ProcessStoppedHandler(object sender, EventArrivedEventArgs e)
         {
             try
             {
-                _processInfos = _dbManager.GetProcessInfo();
+                LoadProcessInfo();
+
                 string processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
                 int processId = Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value);
-                uint exitCode = Convert.ToUInt32(e.NewEvent.Properties["ExitStatus"].Value) == 4294967295 ? 0 : Convert.ToUInt32(e.NewEvent.Properties["ExitStatus"].Value);
+                uint exitCode = 
+                    Convert.ToUInt32(e.NewEvent.Properties["ExitStatus"].Value) == 4294967295
+                    ? 0
+                    : Convert.ToUInt32(e.NewEvent.Properties["ExitStatus"].Value);
 
+                processName = GetNormalizedProcessName(processName);
 
-                foreach (ProcessInfo processInfo in _processInfos)
+                ProcessInfo processInfo = _processInfos.Find(pi => pi.Name == processName);
+
+                if (processInfo != null)
                 {
-                    // 시스템 자체에서 PacketCap_Goos로  들어올때가 있어서 추가 
-                    processName = processName.Split('.')[0] == "PacketCap_Goos" ? "PacketCap_Goose" : processName.Split('.')[0];
+                    Console.WriteLine($"프로세스 종료: {processName}, Pid: {processId}, EXITCODE : {exitCode}");
 
-                    if (processInfo.Name == processName)
-                    {
-                        Console.WriteLine($"프로세스 종료: {processName}, Pid: {processId}, EXITCODE : {exitCode}");
+                    ExitCode exitCodeEnum = exitCode == 0 ? ExitCode.NORMAL : ExitCode.ABNORMAL;
+                    _dbManager.InsertLog(new ProcessEvent(processName, 0, exitCodeEnum));
 
-                        // 종료된 프로세스의 ExitCode를 확인
-                        switch (exitCode)
-                        {
-                            case 0:  // 종료 코드가 0인 경우, 정상 종료
-                                _dbManager.InsertLog(new ProcessEvent(processName, 0, ExitCode.NORMAL));
-                                break;
-                            case 1:   // 종료 코드가 1인 경우, 비정상 종료
-                                _dbManager.InsertLog(new ProcessEvent(processName, 0, ExitCode.ABNORMAL));
-                                break;
-                        }
-
-                        processInfo.Status = 0;
-                        processInfo.Pid = 0;
-                        break;
-                    }
+                    processInfo.Status = 0;
+                    processInfo.Pid = 0;
                 }
 
-                // 프로세스 종료 이벤트가 발생할 때 DB 업데이트
-                _dbManager.UpdateProcessInfo(_processInfos);
-
+                UpdateProcessInfo();
             }
             catch (Exception exception)
             {
                 Console.WriteLine(exception.Message);
                 throw;
             }
-           
+        }
+
+        private void LoadProcessInfo()
+        {
+            if (_processInfos == null)
+            {
+                _processInfos = _dbManager.GetProcessInfo();
+            }
+        }
+
+        private void UpdateProcessInfo()
+        {
+            _dbManager.UpdateProcessInfo(_processInfos);
+        }
+
+        private string GetNormalizedProcessName(string processName)
+        {   // PacketCap_Goos로 가져올 때가 종종있음.  있을때만 Goose로 변경 그외 프로세스는 해당 X
+            return processName.Split('.')[0] == "PacketCap_Goos" ? "PacketCap_Goose" : processName.Split('.')[0];
         }
 
         public enum ExitCode
         {
             NORMAL,
             ABNORMAL
-
         }
-
-
     }
+
 }
