67daea3a12277a393c31d2b7385ff2c1b1285806
vipstar <vipstar_d@naver.com>
2023-09-19 11:42:24 +0900

    특정 Process 종료 추가

--- ExFunction/ServiceController/ServiceController/ServiceControl.cs
+++ ExFunction/ServiceController/ServiceController/ServiceControl.cs
@@ -1,4 +1,6 @@
-﻿namespace ServiceController
+﻿using System.Diagnostics;
+
+namespace ServiceController
 {
     using System.ServiceProcess;
 
@@ -23,9 +25,45 @@
             }
             catch (Exception e)
             {
-                // 예외 처리: 서비스를 다룰 때 오류가 발생할 수 있으므로 필요한 예외 처리를 여기에 추가하세요.
+                
             }
         }
+
+        public void StropProcess()
+        {
+	        // 종료할 프로세스 이름 또는 경로
+	        string[] processName = { "goose_mon, mms_mon" }; // 예: "notepad" 또는 "C:\\Path\\To\\YourApp.exe"
+
+	        // 프로세스 이름 또는 경로를 기반으로 프로세스 찾기
+	        foreach (var p in processName)
+	        {
+				Process[] processes = Process.GetProcessesByName(p);
+
+				if (processes.Length > 0)
+				{
+					// 프로세스가 여러 개일 경우, 모든 프로세스를 종료합니다.
+					foreach (Process process in processes)
+					{
+						try
+						{
+							process.Kill(); // 프로세스 종료
+							process.WaitForExit(); // 종료될 때까지 대기
+							Console.WriteLine($"프로세스 {process.ProcessName} (PID: {process.Id}) 종료됨");
+						}
+						catch (Exception ex)
+						{
+							Console.WriteLine($"프로세스 종료 중 오류 발생: {ex.Message}");
+						}
+					}
+				}
+				else
+				{
+					Console.WriteLine("해당 프로세스가 실행 중이지 않습니다.");
+				}
+
+			}
+
+		}
 
 
     }
